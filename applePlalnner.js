27d680a21fec8319e9258704a8b1864f8390dc53
Dikgang29 <dcmokonyama29@gmail.com>
2022-05-31 15:15:06 +0200

    commit after unit testing

--- applePlalnner.js
+++ applePlalnner.js
@@ -1,6 +1,14 @@
 function SettingsApple(){
-    
-    var marketBoxCost = 0
+    //settings
+    var marketBoxCost = 0;
+    var numberOfApples = 0;
+    var inPacket = 0;
+    var profitPerPack = 0;
+
+    // variables from calcuations
+    var costPerApple = 0;
+    var packsNumber = 0;
+
     function appleBoxCost(boxCost){
         marketBoxCost = boxCost;
 
@@ -9,10 +17,60 @@ function SettingsApple(){
 
         return marketBoxCost;
     }
+
+    function appleInBox(applesNumber){
+        numberOfApples = applesNumber;
+
+    }
+
+    function getApplesInBox(){
+        return numberOfApples;
+
+    }
+    function applesPerPack(applesInpack){
+        inPacket = applesInpack;
+
+
+    }
+    function getApplesPerPack(){
+
+        return inPacket;
+    }
+
+    function applesProfit(packProfit){
+        profitPerPack = packProfit;
+    }
+    function getApplesProfit(){
+
+        return profitPerPack;
+    }
+
+    //calculations
+    function applesCost(){
+        costPerApple = marketBoxCost/numberOfApples;
+    }
     
+    function getAppleCost(){
+        return costPerApple;
+    }
+    function numeberOfPacks(){
+return packsNumber = numberOfApples/inPacket;
+    }
+
+
+
     return{
 
         appleBoxCost,
-        getBoxCost
+        getBoxCost,
+        appleInBox,
+        getApplesInBox,
+        applesPerPack,
+        getApplesPerPack,
+        applesProfit,
+        getApplesProfit,
+        applesCost,
+        getAppleCost,
+        numeberOfPacks
     }
 }
--- test/applesPlanner.tests.js
+++ test/applesPlanner.tests.js
@@ -7,7 +7,85 @@ describe('The Apples Price Planner function' , function(){
 
         assert.equal(50, appleSettings.getBoxCost());
 
-        assert.deepEqual([2,2],[2,2]);
+        // assert.deepEqual([2,2],[2,2]);
+    });
+
+    it('should show be able to enter how many apples in a box e.g 60' , function(){
+
+        let appleSettings = SettingsApple()
+        appleSettings.appleInBox(75);
+
+
+        assert.equal(75, appleSettings.getApplesInBox());
+
+        // assert.deepEqual([2,2],[2,2]);
+    });
+
+    it('should show be able to enter how many apples she can sell in a pack e.g 5 per packet' , function(){
+
+        let appleSettings = SettingsApple()
+        appleSettings.applesPerPack(5);
+
+
+        assert.equal(5, appleSettings.getApplesPerPack());
+
+        // assert.deepEqual([2,2],[2,2]);
+    });
+
+
+    it('should show be able to enter how profit they would like to make e.g R3 profit per pack' , function(){
+
+        let appleSettings = SettingsApple()
+        appleSettings.applesProfit(3);
+
+
+        assert.equal(3, appleSettings.getApplesProfit());
+
+        // assert.deepEqual([2,2],[2,2]);
+    });
+
+    it('should show be able to calculate the price per apple based on the cost price, if box is R50 then 10 apples cost R5' , function(){
+
+        let appleSettings = SettingsApple()
+        appleSettings.appleBoxCost(50);
+        appleSettings.appleInBox(10);
+        appleSettings.applesCost();
+
+
+
+        assert.equal(5, appleSettings.getAppleCost());
+
+        // assert.deepEqual([2,2],[2,2]);
+    });
+
+    it('should show be able to calculate the price per apple based on the cost price, if box is R50 then 10 apples cost R5' , function(){
+
+        let appleSettings = SettingsApple()
+        appleSettings.appleBoxCost(100);
+        appleSettings.appleInBox(5);
+        appleSettings.applesCost();
+
+
+
+        assert.equal(20, appleSettings.getAppleCost());
+
+        // assert.deepEqual([2,2],[2,2]);
+    });
+
+    it('should show be able to calculate the number of box that we can have in a packet eg. ' , function(){
+
+        let appleSettings = SettingsApple();
+        // appleSettings.appleBoxCost(1);
+        appleSettings.appleInBox(20);
+        // appleSettings.applesCost();
+        appleSettings.applesPerPack(2)
+
+
+
+
+        assert.equal(10, appleSettings.numeberOfPacks());
+
+        // assert.deepEqual([2,2],[2,2]);
     });
 
 });
